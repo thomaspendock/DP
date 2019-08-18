0c70011df15379bac255710f6f184b97de8717a8
Thomas <tgp35@cornell.edu>
2019-08-18 12:16:39 -0400

    delete stupid comment

--- int_partition.py
+++ int_partition.py
@@ -1,32 +1,3 @@
-#p(n,m)=p(n-m,m)+p(n,m-1)
-'''
-
-
-
-def accel_asc(n):
-    a = [0 for i in range(n + 1)]
-    k = 1
-    y = n - 1
-    while k != 0:
-        x = a[k - 1] + 1
-        k -= 1
-        while 2 * x <= y:
-            a[k] = x
-            y -= x
-            k += 1
-        l = k + 1
-        while x <= y:
-            a[k] = x
-            a[l] = y
-            yield a[:k + 2]
-            x += 1
-            y -= 1
-        a[k] = x + y
-        y = x + y - 1
-        yield a[:k + 1]
-       
-for i in accel_asc(6): print(i)
-'''
 
 def partition(x):
 
